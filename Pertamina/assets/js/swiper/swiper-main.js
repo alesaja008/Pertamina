744144dc33f678003a94024dfa4f6d9180998d0f
Ales <alesaja008@gmail.com>
2023-10-06 11:28:36 +0700

    test

---
+++ Pertamina/assets/js/swiper/swiper-main.js
@@ -0,0 +1,70 @@
+var swiper = new Swiper(".mySwiper", {
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+
+var swiper = new Swiper(".mySwiper", {
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
+
+// tabs
+var swiperTab = new Swiper(".mySwiperTab", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  freeMode: true,
+  clickable: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 10,
+    },
+  },
+});
+
+// slider Insight
+
+    var swiperProduct = new Swiper(".mySwiperProduct", {
+ 
+      slidesPerView: 1,
+      spaceBetween: 10,
+      pagination: {
+        el: ".swiper-paginationProduct",
+        clickable: true,
+      },
+      breakpoints: {
+        640: {
+          slidesPerView: 1,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        1024: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+      },
+    });
+
+
