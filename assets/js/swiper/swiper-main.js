54ff3a7e6decadc5acfd90180ab0cf2f5d3f0e8f
ALES <alesaja008@gmail.com>
2023-10-11 22:55:42 +0700

    update

--- assets/js/swiper/swiper-main.js
+++ assets/js/swiper/swiper-main.js
@@ -68,3 +68,33 @@ var swiperTab = new Swiper(".mySwiperTab", {
     });
 
 
+
+    // slider logo logo
+
+    var logo = new Swiper(".myLogo", {
+      slidesPerView: 1,
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+      },
+      breakpoints: {
+        320: {
+          slidesPerView: 2,
+          spaceBetween: 10,
+        },
+        640: {
+          slidesPerView: 3,
+          spaceBetween: 10,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+        1024: {
+          slidesPerView: 4,
+          spaceBetween: 15,
+        },
+      },
+    });
+
