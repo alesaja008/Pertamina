500d7f0f0fe2e937c2c1802192109ad8dec4c3b0
Ales <alesaja008@gmail.com>
2023-10-13 10:05:48 +0700

    update

--- assets/js/swiper/swiper-partnership.js
+++ assets/js/swiper/swiper-partnership.js
@@ -1,21 +1,21 @@
 // document.addEventListener("mouseout", disableCardAnimate);
 // document.addEventListener('DOMContentLoaded', animateStatus);
 
-const swiper = new Swiper('.hero-slider', {
-    loop: true,
-    // slidesPerView: 1,
-    // mousewheel: true,
-    // centeredSlides: false,
-    // observer: true,
-    // observeParents: true,
-    navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      pagination: {
-        el: ".swiper-pagination",
-        dynamicBullets: true,
-      },
+const swiper = new Swiper(".hero-slider", {
+  loop: true,
+  // slidesPerView: 1,
+  // mousewheel: true,
+  // centeredSlides: false,
+  // observer: true,
+  // observeParents: true,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    dynamicBullets: true,
+  },
 });
 
 // swiper.on('transitionEnd', function() {
@@ -23,45 +23,57 @@ const swiper = new Swiper('.hero-slider', {
 // });
 
 function changeActiveBg() {
-    const activeElement = document.querySelector('.swiper-slide-active .product-image');
-    const backgroundImage = document.querySelector('#background-image');
-    backgroundImage.animate([{
-        opacity: 0
-    }, {
-        opacity: 1  
-    }], {
-        duration: 300,
-    });
-    // backgroundImage.src = activeElement.src
+  const activeElement = document.querySelector(
+    ".swiper-slide-active .product-image"
+  );
+  const backgroundImage = document.querySelector("#background-image");
+  backgroundImage.animate(
+    [
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+      },
+    ],
+    {
+      duration: 300,
+    }
+  );
+  backgroundImage.src = activeElement.src;
 }
 
-// function cardAnimate(e) {
-//     this.querySelectorAll('.swiper-slide-active .product').forEach(function(boxMove) {
-//         const x = -((window.innerWidth) / 3 - e.pageX) / 90
-//         const y = ((window.innerHeight) / 3 - e.pageY) / 30
-//         boxMove.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)"
-//     });
-//     this.querySelectorAll('.animate-item').forEach(function(itemMove) {
-//         const movingValue = itemMove.getAttribute('data-value');
-//         const x = (window.innerWidth - e.pageX * movingValue) / 230;
-//         const y = (window.innerHeight - e.pageY * movingValue) / 230;
-//         itemMove.style.transform = "translate(" + y + "px," + x + "px)";
-//     });
-// }
+function cardAnimate(e) {
+  this.querySelectorAll(".swiper-slide-active .product").forEach(function (
+    boxMove
+  ) {
+    const x = -(window.innerWidth / 3 - e.pageX) / 90;
+    const y = (window.innerHeight / 3 - e.pageY) / 30;
+    boxMove.style.transform = "rotateY(" + x + "deg) rotateX(" + y + "deg)";
+  });
+  this.querySelectorAll(".animate-item").forEach(function (itemMove) {
+    const movingValue = itemMove.getAttribute("data-value");
+    const x = (window.innerWidth - e.pageX * movingValue) / 230;
+    const y = (window.innerHeight - e.pageY * movingValue) / 230;
+    itemMove.style.transform = "translate(" + y + "px," + x + "px)";
+  });
+}
 
-// function disableCardAnimate(d) {
-//     this.querySelectorAll('.swiper-slide-active .product').forEach(function(boxMove) {
-//         boxMove.style.transform = "rotateY(0deg) rotateX(0deg)"
-//     });
-//     this.querySelectorAll('.header .animate-item').forEach(function(itemMove) {
-//         itemMove.style.transform = "translate(0px, 0px)"
-//     });
-// }
+function disableCardAnimate(d) {
+  this.querySelectorAll(".swiper-slide-active .product").forEach(function (
+    boxMove
+  ) {
+    boxMove.style.transform = "rotateY(0deg) rotateX(0deg)";
+  });
+  this.querySelectorAll(".header .animate-item").forEach(function (itemMove) {
+    itemMove.style.transform = "translate(0px, 0px)";
+  });
+}
 
-// function animateStatus() {
-//     if (window.innerWidth > 576) {
-//         document.addEventListener("mousemove", cardAnimate);
-//     }
-// }
+function animateStatus() {
+  if (window.innerWidth > 576) {
+    document.addEventListener("mousemove", cardAnimate);
+  }
+}
 
 window.onload = changeActiveBg();
