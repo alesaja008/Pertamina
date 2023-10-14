2e651696382a7623a7881b47c4593ff81ea49619
Ales <alesaja008@gmail.com>
2023-10-14 09:41:44 +0700

    home page dan page LTC

--- assets/js/swiper/swiper-main.js
+++ assets/js/swiper/swiper-main.js
@@ -43,58 +43,82 @@ var swiperTab = new Swiper(".mySwiperTab", {
 
 // slider Insight
 
-    var swiperProduct = new Swiper(".mySwiperProduct", {
- 
+var swiperProduct = new Swiper(".mySwiperProduct", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-paginationProduct",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
       slidesPerView: 1,
       spaceBetween: 10,
-      pagination: {
-        el: ".swiper-paginationProduct",
-        clickable: true,
-      },
-      breakpoints: {
-        640: {
-          slidesPerView: 1,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        1024: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-      },
-    });
-
-
-
-    // slider logo logo
-
-    var logo = new Swiper(".myLogo", {
-      slidesPerView: 1,
+    },
+    768: {
+      slidesPerView: 2,
       spaceBetween: 10,
-      navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-      },
-      breakpoints: {
-        320: {
-          slidesPerView: 2,
-          spaceBetween: 10,
-        },
-        640: {
-          slidesPerView: 3,
-          spaceBetween: 10,
-        },
-        768: {
-          slidesPerView: 4,
-          spaceBetween: 15,
-        },
-        1024: {
-          slidesPerView: 4,
-          spaceBetween: 15,
-        },
-      },
-    });
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+  },
+});
 
+// slider logo logo
+
+var logo = new Swiper(".myLogo", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 10,
+    },
+    640: {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+  },
+});
+
+// LTC slider
+var swiperContent = new Swiper(".mySwiperLtc", {
+  slidesPerView: 1,
+  spaceBetween: 10,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    1024: {
+      slidesPerView: 5,
+      spaceBetween: 50,
+    },
+  },
+});
